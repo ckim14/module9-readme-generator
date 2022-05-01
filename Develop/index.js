9454f5b405fc0adbcf7b0e9118349a38ee466551
ckim14 <chikakim@gmail.com>
2022-05-01 16:58:13 -0400

    added node modules and functions to generate write readme and init app

--- Develop/index.js
+++ Develop/index.js
@@ -1,100 +1,119 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    title: "github",
-    message: "What is your Github username? (Required)",
-    validate: (githubInput) => {
-      if (githubInput) {
-        return true;
-      } else {
-        console.log("Please enter your GitHub username!");
-        return false;
-      }
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      title: "github",
+      message: "What is your Github username? (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    title: "email",
-    message: "What is your email address? (Required)",
-    validate: (githubInput) => {
-      if (githubInput) {
-        return true;
-      } else {
-        console.log("Please enter your email address!");
-        return false;
-      }
+    {
+      type: "input",
+      title: "email",
+      message: "What is your email address? (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your email address!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    title: "title",
-    message: "What is the title of your project? (Required)",
-    validate: (titleInput) => {
-      if (titleInput) {
-        return true;
-      } else {
-        console.log("Please enter a title for your project.");
-        return false;
-      }
+    {
+      type: "input",
+      title: "title",
+      message: "What is the title of your project? (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter a title for your project.");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Tell us a little more about your project.",
-  },
-  {
-    type: "input",
-    name: "installation instructions",
-    message: "How do you install your project?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Give some usage information about your project.",
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message: "Any contribution guidelines we should know about?",
-  },
-  {
-    type: "input",
-    name: "testinstructions",
-    message: "How should we test?",
-  },
-  {
-    type: "checkbox",
-    name: "license",
-    message: "Pick your license.",
-    choices: ["1", "2", "3"],
-  },
-];
+    {
+      type: "input",
+      name: "description",
+      message: "Tell us a little more about your project.",
+    },
+    {
+      type: "input",
+      name: "installation instructions",
+      message: "How do you install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use.",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Any contribution guidelines we should know about?",
+    },
+    {
+      type: "input",
+      name: "testinstructions",
+      message: "How should we test?",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Pick your license.",
+      choices: ["MIT", "GNU GPLv3", "Apache", "Mozilla", "Boost", "Unlicense"],
+    },
+  ]);
+};
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+questions().then();
+
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("You can now see your README file.");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(function (userInput) {
+    console.log(userInput);
+    writeToFile("README.md", generateMarkdown(userInput));
+  });
+}
 
 // Function call to initialize app
 init();
